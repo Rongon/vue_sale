424174a1802ac855f26cf462535598e10421a7a9
rongon <rongon@zzl.com>
2021-11-21 21:21:57 +0800

    完成登录部分

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+  <div>
+    <el-button type="warning" round @click="loginOut">离开</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    loginOut() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
